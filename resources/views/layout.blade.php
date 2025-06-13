cea4f9c7cc0883731fd340c3ed3a23b921b3648e
loekvandeloo <1206994@student.roc-nijmegen.nl>
2025-06-13 13:50:37 +0200

    added css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html lang="nl">
+
 <head>
     <meta charset="UTF-8">
     <title>Tic-Tac-Toe</title>
-    <style>
-        body { font-family: sans-serif; padding: 20px; }
-        nav a { margin-right: 15px; }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
+
 <body>
     <nav>
         <a href="{{ route('games.index') }}">Spellen</a>
         <a href="{{ route('games.create') }}">Nieuw spel</a>
         <a href="{{ route('players.index') }}">Spelers</a>
         <a href="{{ route('players.create') }}">Speler aanmaken</a>
-
     </nav>
     <hr>
-    
+
     @yield('content')
 </body>
+
 </html>
